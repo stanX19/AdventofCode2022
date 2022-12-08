e452f054bd116d513110cf6161738022acfb927b
stan <tanshanchien@gmail.com>
2022-12-08 12:46:47 +0800

    6th day cleanup

--- 6th_day/first_unique14.cpp
+++ 6th_day/first_unique14.cpp
@@ -1,60 +1,54 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <deque>
 
-
-bool    all_unique(std::deque<char> str, size_t n){
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = i + 1; j < n; j++) {
-            //std::cout << "i: " << str[i] << " | j: " << str[j] << std::endl;
-            if (str[i] == str[j])
-                return 0;
-        }
-    }
-    return 1;
-}
-
-size_t  first_n_unique_char(std::string line, size_t n)
+bool all_unique(const std::string& str, size_t start_idx, size_t n)
 {
-	std::deque<char>    buf;
-    char                c;
-	size_t			    idx;
-
-	idx = 0;
-	while (line[idx])
-	{
-        c = line[idx];
-        buf.push_back(c);
-        //std::cout << "running: " << c << std::endl;
-        if (buf.size() > n) {
-            buf.pop_front();
-            if (all_unique(buf, n)){
-	            return idx;
+    for (size_t i = start_idx; i < start_idx + n; i++)
+    {
+        for (size_t j = i + 1; j < start_idx + n; j++)
+        {
+            if (str[i] == str[j])
+            {
+                return false;
             }
         }
-		idx++;
-	}
-	return -1;
+    }
+    return true;
 }
 
+size_t first_n_unique_char(const std::string& line, size_t n)
+{
+    for (size_t i = 0; i < line.size(); i++)
+    {
+        if (all_unique(line, i, n))
+        {
+            return i + n;
+        }
+    }
+    return -1;
+}
 
 int main(int argc, char** argv)
 {
-	std::string     path;
-    std::string     line;
-    std::ifstream   inFile;
-	size_t          index;
+    std::string path;
+    std::string line;
+    std::ifstream inFile;
 
-	if (argc == 1)
-		path = "files/input.txt";
-	else
-		path = argv[1];
-    
+    if (argc == 1)
+    {
+        path = "files/input.txt";
+    }
+    else
+    {
+        path = argv[1];
+    }
+
     inFile.open(path);
-    while (!inFile.eof()){
+    while (!inFile.eof())
+    {
         std::getline(inFile, line);
-        index = first_n_unique_char(line, 14) + 1;
+        size_t index = first_n_unique_char(line, 14);
         std::cout << index << "\n";
     }
     inFile.close();
